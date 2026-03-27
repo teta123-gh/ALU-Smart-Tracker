02dcbbc85b5f802a399b53129e6657f27c83256c
teta123-gh <k.teta1@alustudent.com>
2026-03-27 06:41:51 +0200

    Update app run command

--- backend/app.py
+++ backend/app.py
@@ -45,4 +45,4 @@ def create_app():
 if __name__ == '__main__':
     app = create_app()
     print("ALU Tracker API running on http://localhost:5000")
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
